94c4d8d49e87301689c10a434869ce3846daf335
GangOn0215 <ekdldktnekf@gmail.com>
2022-04-05 16:41:26 +0900

    모듈화(module), component로 분리

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,74 +1,11 @@
 import React, { useState, useEffect, useContext } from 'react';
 
 /* Components */
-import MyHeader from '../components/MyHeader';
-import MyButton from '../components/MyButton';
+import SetHeader from '../components/SetHeader';
 import DiaryList from '../components/DiaryList';
 
 /* Context */
 import { DiaryStateContext } from '../App';
-
-// increase, decrease
-const indecreasePeriodicity = (periodicity, curDate, onChangeDate) => {
-  const switchPeriodicity = periodicity;
-  let increase = null;
-  let decrease = null;
-
-  switch (switchPeriodicity) {
-    case 'daily':
-      increase = () =>
-        onChangeDate(new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getDate() + 1));
-      decrease = () =>
-        onChangeDate(new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getDate() - 1));
-      break;
-    case 'weekly':
-      increase = () =>
-        onChangeDate(new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getDate() + 7));
-      decrease = () =>
-        onChangeDate(new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getDate() - 7));
-      break;
-    case 'monthly':
-      increase = () => onChangeDate(new Date(curDate.getFullYear(), curDate.getMonth() + 1));
-      decrease = () => onChangeDate(new Date(curDate.getFullYear(), curDate.getMonth() - 1));
-      break;
-    default:
-      break;
-  }
-
-  return { increase, decrease };
-};
-
-const SetHeader = ({ periodicity, headTextOBJ, curDate, onChangeDate }) => {
-  let headText = null;
-
-  const { increase, decrease } = indecreasePeriodicity(periodicity, curDate, onChangeDate);
-
-  switch (periodicity) {
-    case 'diary':
-      headText = headTextOBJ.dailyText;
-      break;
-    case 'weekly':
-      headText = headTextOBJ.weeklyText;
-      break;
-    case 'monthly':
-      headText = headTextOBJ.monthlyText;
-      break;
-    default:
-      headText = headTextOBJ.dailyText;
-      break;
-  }
-
-  return (
-    <MyHeader
-      headText={headText}
-      leftChild={<MyButton text={'<'} onClick={decrease} />}
-      rightChild={<MyButton text={'>'} onClick={increase} />}
-      periodicity={periodicity}
-      curDate={curDate}
-      onChangeDate={onChangeDate}
-    />
-  );
-};
 
 const Home = () => {
   const diaryLists = useContext(DiaryStateContext);
